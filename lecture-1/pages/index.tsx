632a6035bf543fd61886083e5a68438efb3e7800
David K. Kim <rlaruddlf711@naver.com>
2023-01-29 22:55:27 +0900

    Add store mock data

--- lecture-1/pages/index.tsx
+++ lecture-1/pages/index.tsx
@@ -1,12 +1,25 @@
-import { Fragment } from 'react';
-import Header from '../components/common/Header';
-import styles from '../styles/header.module.scss';
+import { Fragment, useEffect } from 'react';
 import Link from 'next/link';
+import { NextPage } from 'next';
 import { VscFeedback } from 'react-icons/vsc';
 import { AiOutlineShareAlt } from 'react-icons/ai';
-import MapSection from '@/components/home/MapSection';
+import Header from '../components/common/Header';
+import styles from '../styles/header.module.scss';
+import MapSection from '../components/home/MapSection';
+import { Store } from '../types/store';
+import useStores from '../hooks/useStores';
 
-export default function Home() {
+interface Props {
+  stores: Store[];
+}
+
+const Home: NextPage<Props> = ({ stores }) => {
+  const { initializeStores } = useStores();
+
+  useEffect(() => {
+    initializeStores(stores);
+  }, [initializeStores, stores]);
+
   return (
     <Fragment>
       <Header
@@ -31,4 +44,15 @@ export default function Home() {
       </main>
     </Fragment>
   );
+};
+export default Home;
+
+export async function getStaticProps() {
+  /** TODO: next api routes로 불러오기 */
+  const stores = (await import('../public/stores.json')).default;
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
