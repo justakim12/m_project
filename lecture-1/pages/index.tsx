689fb94c7791f6dcf473a812d51bb2bcc4844777
David K. Kim <rlaruddlf711@naver.com>
2023-02-04 12:34:00 +0900

    Add share event and going back to home when pressing logo

--- lecture-1/pages/index.tsx
+++ lecture-1/pages/index.tsx
@@ -1,11 +1,7 @@
 import { Fragment, useEffect } from 'react';
-import Link from 'next/link';
 import { NextPage } from 'next';
-import { VscFeedback } from 'react-icons/vsc';
-import { AiOutlineShareAlt } from 'react-icons/ai';
-import Header from '../components/common/Header';
-import styles from '../styles/header.module.scss';
 import MapSection from '../components/home/MapSection';
+import Header from '../components/home/Header';
 import { Store } from '../types/store';
 import useStores from '../hooks/useStores';
 
@@ -22,23 +18,7 @@ const Home: NextPage<Props> = ({ stores }) => {
 
   return (
     <Fragment>
-      <Header
-        rightElements={[
-          <button
-            onClick={() => {
-              alert('복사!');
-            }}
-            className={styles.box}
-            style={{ marginRight: 8 }}
-            key="button"
-          >
-            <AiOutlineShareAlt size={20} />
-          </button>,
-          <Link href="/feedback" className={styles.box} key="link">
-            <VscFeedback size={20} />
-          </Link>,
-        ]}
-      />
+      <Header />
       <main style={{ width: '100%', height: '100%' }}>
         <MapSection />
       </main>
