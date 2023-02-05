9f348116d69d3abbd05fbf6ace4c5b5cced38b73
David K. Kim <rlaruddlf711@naver.com>
2023-02-05 11:30:43 +0900

    Add share button to details for shop

--- lecture-1/pages/[name].tsx
+++ lecture-1/pages/[name].tsx
@@ -1,9 +1,10 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import type { Store } from '../types/store';
 import styles from '../styles/detail.module.scss';
-
+import DetailHeader from '../components/home/DetailHeader';
 import DetailContent from '../components/home/DetailContent';
-import DetailHeader from '@/components/home/DetailHeader';
+import { useRouter } from 'next/router';
+import useCurrentStore from '../hooks/useCurrentStore';
 
 interface Props {
   store: Store;
@@ -12,12 +13,22 @@ interface Props {
 const StoreDetail: NextPage<Props> = ({ store }) => {
   const expanded = true;
 
+  const router = useRouter();
+  const { setCurrentStore } = useCurrentStore();
+
+  const goToMap = () => {
+    setCurrentStore(store);
+    router.push(`
+      /?zoom=15&lat=${store.coordinates[0]}&lng=${store.coordinates[1]}
+    `);
+  };
+
   return (
     <div className={`${styles.detailSection} ${styles.expanded}`}>
       <DetailHeader
         currentStore={store}
         expanded={expanded}
-        onClickArrow={() => null}
+        onClickArrow={goToMap}
       />
       <DetailContent currentStore={store} expanded={expanded} />
     </div>
