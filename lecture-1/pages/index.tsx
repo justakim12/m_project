eb50bd4320005f445a302264e48ac94bf3f41495
David K. Kim <rlaruddlf711@naver.com>
2023-02-05 10:52:21 +0900

    Add details to shop

--- lecture-1/pages/index.tsx
+++ lecture-1/pages/index.tsx
@@ -1,7 +1,8 @@
 import { Fragment, useEffect } from 'react';
 import { NextPage } from 'next';
-import MapSection from '../components/home/MapSection';
 import Header from '../components/home/Header';
+import MapSection from '../components/home/MapSection';
+import DetailSection from '../components/home/DetailSection';
 import { Store } from '../types/store';
 import useStores from '../hooks/useStores';
 
@@ -19,8 +20,16 @@ const Home: NextPage<Props> = ({ stores }) => {
   return (
     <Fragment>
       <Header />
-      <main style={{ width: '100%', height: '100%' }}>
+      <main
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          overflow: 'hidden',
+        }}
+      >
         <MapSection />
+        <DetailSection />
       </main>
     </Fragment>
   );
