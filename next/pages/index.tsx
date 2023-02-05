8323e4b9723f914d9c205ce8992b536c56e05e2e
David K. Kim <rlaruddlf711@naver.com>
2023-02-05 17:41:19 +0900

    Rollback using API route

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -8,47 +8,46 @@ import useStores from '../hooks/useStores';
 import { NextSeo } from 'next-seo';
 
 interface Props {
-  stores: Store[];
+    stores: Store[];
 }
 
-const Home: NextPage<Props> = ({ stores }) => {
-  const { initializeStores } = useStores();
+const Home: NextPage<Props> = ({stores}) => {
+    const {initializeStores} = useStores();
 
-  useEffect(() => {
-    initializeStores(stores);
-  }, [initializeStores, stores]);
+    useEffect(() => {
+        initializeStores(stores);
+    }, [initializeStores, stores]);
 
-  return (
-    <Fragment>
-      <NextSeo
-        title="매장 지도"
-        description="Next.js 시작하기 강의를 위한 매장 지도 서비스입니다."
-        canonical="https://inflearn-nextjs.vercel.app"
-      />
-      <Header />
-      <main
-        style={{
-          position: 'relative',
-          width: '100%',
-          height: '100%',
-          overflow: 'hidden',
-        }}
-      >
-        <MapSection />
-        <DetailSection />
-      </main>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <NextSeo
+                title="매장 지도"
+                description="Next.js 시작하기 강의를 위한 매장 지도 서비스입니다."
+                canonical="https://inflearn-nextjs.vercel.app"
+            />
+            <Header/>
+            <main
+                style={{
+                    position: 'relative',
+                    width: '100%',
+                    height: '100%',
+                    overflow: 'hidden',
+                }}
+            >
+                <MapSection/>
+                <DetailSection/>
+            </main>
+        </Fragment>
+    );
 };
 export default Home;
 
 export async function getStaticProps() {
-  const stores = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
-  ).then((response) => response.json());
+    /** TODO: next api routes로 불러오기 */
+    const stores = (await import('../public/stores.json')).default;
 
-  return {
-    props: { stores },
-    revalidate: 60 * 60,
-  };
+    return {
+        props: {stores},
+        revalidate: 60 * 60,
+    };
 }
